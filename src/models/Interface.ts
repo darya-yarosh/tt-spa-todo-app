ba46775e9b2fcd00b8694abc356dffe70b8fae1b
darya-yarosh <gurimusagi@yandex.ru>
2023-09-26 11:56:54 +0300

    Created model of interface constants

---
+++ src/models/Interface.ts
@@ -0,0 +1,27 @@
+export const enum BUTTON {
+    open = "Open",
+    save = "Save",
+    edit = "Edit",
+    delete = "Delete",
+    return = "Return",
+    add = "Add",
+    cancel = "Cancel",
+    signIn = "Sign in",
+    logIn = "Log in",
+    reply = "Reply",
+    send = "Send",
+}
+
+export const enum OPTION {
+    default = " -- Select option -- ",
+}
+
+export const enum AppInfo {
+    name = "TODO-APP",
+    author = "Darya Yarosh",
+}
+
+export const enum PageList {
+    projects = "Project List",
+    tasks = "Task List"
+}
