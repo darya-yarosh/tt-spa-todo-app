d2b07d10874091baf3aae1307431857f9fbd06e6
darya-yarosh <gurimusagi@yandex.ru>
2023-09-27 14:58:33 +0300

    Updated Project Note

--- src/components/Project/ProjectNote/ProjectNote.tsx
+++ src/components/Project/ProjectNote/ProjectNote.tsx
@@ -1,30 +1,36 @@
-import { BUTTON } from 'models/Interface';
+import { useContext } from 'react';
+
 import Project from 'models/Project';
+import { BUTTON } from 'models/Interface';
 
 import ButtonIcon from 'components/General/ButtonIcon/ButtonIcon';
 
 import editIcon from "images/buttons/edit.svg";
 import removeIcon from "images/buttons/remove.svg";
 
-import "components/Project/ProjectNote/ProjectNote.scss"
+import styles from "components/Project/ProjectNote/ProjectNote.module.scss"
+
+import { OpenProjectContext } from 'App';
 
 interface ProjectNoteProps {
     project: Project;
+    removeProject: (projectID: string) => void;
 }
 
-export default function ProjectNote({ project }: ProjectNoteProps) {
-    function handlerOpenPageProject() {
-        // 
-    }
+export default function ProjectNote({
+    project,
+    removeProject
+}: ProjectNoteProps) {
+    const openProjectFunc = useContext(OpenProjectContext)
 
     function handlerRemoveProject() {
-        //
+        removeProject(project.id);
     }
 
-    return <div className="project-wrapper">
-        <span>
-            <ButtonIcon iconSVG={editIcon} caption={BUTTON.edit} onClick={handlerOpenPageProject} />
-            <p>{project.title}</p>
+    return <div className={styles.wrapper}>
+        <span className={styles.span}>
+            <ButtonIcon iconSVG={editIcon} caption={BUTTON.edit} onClick={() => openProjectFunc(project)} />
+            <p className={styles.title}>{project.title}</p>
         </span>
         <span>
             <ButtonIcon iconSVG={removeIcon} caption={BUTTON.delete} onClick={handlerRemoveProject} />
