58eb8eb6785133b277068e16203be0d72ce7eb9e
darya-yarosh <gurimusagi@yandex.ru>
2023-09-27 14:17:33 +0300

    Updated Header

--- src/components/General/Header/Header.tsx
+++ src/components/General/Header/Header.tsx
@@ -1,7 +1,7 @@
 import { AppInfo } from "models/Interface";
 
-import "components/General/Header/Header.scss";
+import styles from "components/General/Header/Header.module.scss";
 
 export default function Header() {
-    return <header><p>{AppInfo.name}</p></header>
+    return <header className={styles.header}><p className={styles.title}>{AppInfo.name}</p></header>
 }
