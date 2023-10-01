a57225f5636094f75f6f684b6b2ebe400015e51d
darya-yarosh <gurimusagi@yandex.ru>
2023-10-02 02:31:28 +0300

    Created MOCK object for storage

---
+++ src/models/mockStorage.ts
@@ -0,0 +1,55 @@
+import Project from "models/Project";
+import Storage from "models/Storage";
+import { Priority, TaskStatus } from "models/Task";
+
+const MOCK_PROJECTLIST: Project[] = [{
+    id: crypto.randomUUID(),
+    title: "Project #1",
+    tasks: [{
+        id: crypto.randomUUID(),
+        number: 1,
+        title: "Title of task",
+        desc: "Description of the task. This description is very-very long, you can trust me.",
+        dataCreated: new Date().toLocaleDateString('ru-RU'),
+        workingHours: 40,
+        dateEnded: "",
+        priority: Priority.high,
+        status: TaskStatus.queue,
+        subtasks: [],
+        attachedFiles: [],
+        comments: [],
+    },
+    {
+        id: crypto.randomUUID(),
+        number: 2,
+        title: "Some task",
+        desc: "This task is under development.",
+        dataCreated: new Date().toLocaleDateString('ru-RU'),
+        workingHours: 30,
+        dateEnded: "",
+        priority: Priority.medium,
+        status: TaskStatus.development,
+        subtasks: [],
+        attachedFiles: [],
+        comments: [],
+    },
+    {
+        id: crypto.randomUUID(),
+        number: 3,
+        title: "Create css-theme changer for app",
+        desc: "",
+        dataCreated: new Date().toLocaleDateString('ru-RU'),
+        workingHours: 20,
+        dateEnded: new Date().toLocaleDateString('ru-RU'),
+        priority: Priority.low,
+        status: TaskStatus.done,
+        subtasks: [],
+        attachedFiles: [],
+        comments: [],
+    },
+]
+}]
+
+export const MOCK_STORAGE: Storage = {
+    projects: MOCK_PROJECTLIST
+}
