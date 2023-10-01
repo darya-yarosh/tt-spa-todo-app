5ead8aa77d024c6147a4598f012ed8e1f19f9a4d
darya-yarosh <gurimusagi@yandex.ru>
2023-10-02 02:51:48 +0300

    Created form component for comment list

---
+++ src/components/Comment/CommentListForm/CommentListForm.tsx
@@ -0,0 +1,52 @@
+import { Comment } from "models/Comment";
+
+import CommentForm from "components/Comment/CommentForm/CommentForm";
+import CommentExistingForm from "components/Comment/CommentExistingForm/CommentExistingForm";
+
+import styles from "components/Comment/CommentListForm/CommentListForm.module.scss";
+
+interface CommentListFormProps {
+    comments: Comment[],
+    updateComments: (comments: Comment[]) => void,
+}
+
+export default function CommentListForm({
+    comments,
+    updateComments
+}: CommentListFormProps) {
+    function handlerAddComment(commentValue: string) {
+        const newComment: Comment = {
+            id: crypto.randomUUID(),
+            value: commentValue,
+            subComments: []
+        }
+
+        const updatedComments = [...comments, newComment];
+        updateComments(updatedComments)
+    }
+
+    function handlerUpdateComment(updatedComment: Comment) {
+        const updatedCommentIndex = comments.findIndex(comment=>comment.id===updatedComment.id);
+        const updatedComments = [...comments];
+        
+        updatedComments[updatedCommentIndex]=updatedComment;
+        updateComments(updatedComments)
+    }
+
+    function handlerRemoveComment(commentId: string) {
+        const updatedCommentIndex = comments.findIndex(comment=>comment.id===commentId);
+        const updatedComments = [...comments];
+        
+        updatedComments.splice(updatedCommentIndex, 1);
+        updateComments(updatedComments)
+    }
+
+    return <div className={styles.wrapper}>
+        <div className={styles.commentsWrapper}>
+        {comments.map(comment =>
+            <CommentExistingForm key={comment.id} comment={comment} updateComment={handlerUpdateComment} removeComment={handlerRemoveComment}/>
+        )}
+        </div>
+        {<CommentForm sendForm={handlerAddComment} />}
+    </div>
+}
