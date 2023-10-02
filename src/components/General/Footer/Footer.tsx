8497427b71dbf1f1faac4bfcfe3e2c5aeb0fa797
darya-yarosh <gurimusagi@yandex.ru>
2023-10-02 03:06:11 +0300

    Updated footer component. Added theme changer.

--- src/components/General/Footer/Footer.tsx
+++ src/components/General/Footer/Footer.tsx
@@ -1,9 +1,37 @@
-import { AppInfo } from "models/Interface";
+import { useState } from "react";
+
+import { AppInfo, Theme } from "models/Interface";
+
+import ButtonIcon from "components/General/ButtonIcon/ButtonIcon";
+import themeIcon from "images/buttons/theme.svg";
 
 import styles from "components/General/Footer/Footer.module.scss";
 
 export default function Footer() {
+    const [currentTheme, setCurrentTheme] = useState<Theme>("white")
+
+    function changeTheme() {
+        currentTheme === "dark" ? setWhiteTheme() : setBlackTheme()
+    }
+
+    function setBlackTheme() {
+        const appElement = document.getElementById('app');
+
+        if (appElement !== null) appElement.classList.add('dark-theme');
+        setCurrentTheme("dark");
+    }
+    function setWhiteTheme() {
+        const appElement = document.getElementById('app');
+
+        if (appElement !== null) appElement.classList.remove('dark-theme');
+        setCurrentTheme("white");
+    }
+
     return <footer className={styles.wrapper}>
-        <p className={styles.author}>{AppInfo.author}</p>
+        <span className={styles.line} />
+        <section>
+            <p className={styles.author}>{AppInfo.author}</p>
+            <ButtonIcon iconSVG={themeIcon} caption={"Button for change theme style."} onClick={changeTheme} />
+        </section>
     </footer>
 }
