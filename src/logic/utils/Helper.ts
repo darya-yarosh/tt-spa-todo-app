c2a562a186c68ac43a62537b0d051e04d80b4801
darya-yarosh <gurimusagi@yandex.ru>
2023-10-02 12:44:03 +0300

    Removed the lowercase conversion of the filter for the task number

--- src/logic/utils/Helper.ts
+++ src/logic/utils/Helper.ts
@@ -2,7 +2,7 @@ import Task from "models/Task";
 
 export function getFilteredTasks(taskList: Task[], filter: string) {
     return taskList.filter((task: Task) =>
-        task.number.toString().includes(filter.toLowerCase())
+        task.number.toString().includes(filter)
         || task.title.toLowerCase().includes(filter.toLowerCase())
     );
 }
