3b30bf3ebd84642694ed7fed9aacef94c1fbfc30
darya-yarosh <gurimusagi@yandex.ru>
2023-10-02 03:00:49 +0300

    Updated component of project list page

--- src/components/Pages/ProjectListPage/ProjectListPage.tsx
+++ src/components/Pages/ProjectListPage/ProjectListPage.tsx
@@ -3,60 +3,63 @@ import { useContext } from "react";
 import Project from "models/Project";
 import { FormOperation, PageList } from "models/Interface";
 
-import Modal from "components/General/Modal/Modal";
-import ButtonIcon from "components/General/ButtonIcon/ButtonIcon";
 import ProjectForm from "components/Project/ProjectForm/ProjectForm";
 import ProjectNote from "components/Project/ProjectNote/ProjectNote";
 
-import useToggle from "logic/utils/use-toggle";
-
+import ButtonIcon from "components/General/ButtonIcon/ButtonIcon";
 import plusIcon from "images/buttons/plus.svg";
+
+import ProjectController from "logic/storage/ProjectController";
 
 import styles from "components/Pages/ProjectListPage/ProjectListPage.module.scss";
 
-import { ProjectListContext } from "App";
+import { ModalContext, UpdateStorageContext } from "App";
 
-export default function ProjectListPage() {
-    const projectListContext = useContext(ProjectListContext);
-    const [isModalOpen, toggleIsModalOpen] = useToggle(false);
+interface ProjectListProps {
+    projectList: Project[];
+}
 
-    function handlerSendFormProject(projectTitle: string) {
-        const newProject: Project = {
-            id: crypto.randomUUID(),
-            title: projectTitle,
-            tasks: []
-        }
+export default function ProjectListPage({
+    projectList
+}: ProjectListProps) {
+    const modal = useContext(ModalContext);
 
-        const updatedProjects = [...projectListContext.projectList, newProject];
-        projectListContext.setProjectList(updatedProjects);
+    const updateStorageContext = useContext(UpdateStorageContext)
+
+    function handlerOpenProjectForm() {
+        modal.setContent(<ProjectForm operationTitle={FormOperation.create}
+            sendForm={handlerSendFormProject}
+            closeForm={modal.toggle} />)
+        modal.toggle();
+    }
+
+    async function handlerSendFormProject(newProject: Project) {
+        const projectController = new ProjectController();
+        await projectController.createProject(newProject).then(updateStorageContext);
     }
 
     function handlerRemoveProject(removeId: string) {
-        const updatedProjects = projectListContext.projectList.filter(project => project.id !== removeId);
-        projectListContext.setProjectList(updatedProjects);
+        const projectController = new ProjectController();
+        projectController.removeProject(removeId).then(updateStorageContext);
+
     }
 
-    return <>
+    return <div className={styles.wrapper}>
         <div className={styles.header}>
             <p className={styles.title}>{PageList.projects}</p>
-            <ButtonIcon iconSVG={plusIcon} caption={"Button for adding new project."} onClick={(toggleIsModalOpen as () => void)} />
+            <ButtonIcon iconSVG={plusIcon} caption={"Button for adding new project."} onClick={handlerOpenProjectForm} />
         </div>
         <span className={styles.line} />
         <div className={styles.content}>
             <div className={styles.projectGrid}>
-                {projectListContext.projectList.map(project =>
-                    <span className={styles.span}>
-                        <ProjectNote key={project.id} project={project} removeProject={handlerRemoveProject} />
+                {projectList.map(project =>
+                    <span key={project.id} className={styles.span}>
+                        <ProjectNote key={project.id}
+                            project={project}
+                            removeProject={handlerRemoveProject} />
                     </span>
                 )}
             </div>
         </div>
-        {isModalOpen && (
-            <Modal handleDismiss={toggleIsModalOpen as ()=>void}>
-                <ProjectForm operationTitle={FormOperation.create}
-                    sendForm={handlerSendFormProject}
-                    closeForm={(toggleIsModalOpen as () => void)} />
-            </Modal>
-        )}
-    </>
+    </div>
 }
