bb3962aae826c585d09bf32040fffc3458918493
darya-yarosh <gurimusagi@yandex.ru>
2023-10-02 14:37:34 +0300

    Added error output when loading/saving projects and tasks from storage fails

--- src/logic/storage/ProjectController.ts
+++ src/logic/storage/ProjectController.ts
@@ -5,8 +5,9 @@ import StorageController from "logic/storage/StorageController";
 
 export class ProjectControllerClass {
     async createProject(newProject: Project) {
-        const updatedProjectList = await this.getProjectList();
         if (newProject.id === "") newProject.id = crypto.randomUUID();
+
+        const updatedProjectList = await this.getProjectList();
         updatedProjectList.push(newProject)
         await this.setProjectList(updatedProjectList);
     }
@@ -18,7 +19,7 @@ export class ProjectControllerClass {
             window.alert("The project with the selected ID was not found.")
             return;
         }
-        
+
         updatedProjectList.splice(projectIndex, 1)
         await this.setProjectList(updatedProjectList);
     }
@@ -26,12 +27,21 @@ export class ProjectControllerClass {
     async getProject(projectId: string) {
         const projectList = await this.getProjectList();
         const project = projectList.find(project => project.id === projectId);
-        return project as Project;
+        if (project === undefined) {
+            window.alert("The project with the selected ID was not found.")
+            return;
+        }
+        return project;
     }
 
     async setProject(projectId: string, updatedProject: Project) {
         const updatedProjectList = await this.getProjectList();
         const projectIndex = updatedProjectList.findIndex(project => project.id === projectId);
+        if (projectIndex === -1) {
+            window.alert("The project with the selected ID was not found.")
+            return;
+        }
+
         updatedProjectList[projectIndex] = updatedProject;
         await this.setProjectList(updatedProjectList);
     }
--- src/logic/storage/TaskController.ts
+++ src/logic/storage/TaskController.ts
@@ -5,6 +5,10 @@ import ProjectController from "logic/storage/ProjectController";
 export class TaskControllerClass {
     async createTask(projectId: string, newTask: Task) {
         const updatedProject = await ProjectController.getProject(projectId);
+        if (updatedProject === undefined) {
+            window.alert("An error occurred while trying to create the task.")
+            return;
+        };
 
         if (newTask.id === "") newTask.id = crypto.randomUUID();
         const lastProjectTask = updatedProject.tasks[updatedProject.tasks.length - 1];
@@ -16,6 +20,11 @@ export class TaskControllerClass {
 
     async removeTask(projectId: string, taskId: string) {
         const updatedProject = await ProjectController.getProject(projectId);
+        if (updatedProject === undefined) {
+            window.alert("An error occurred while trying to remove the task.")
+            return;
+        };
+
         const taskIndex = updatedProject.tasks.findIndex(task => task.id === taskId);
         if (taskIndex === -1) {
             window.alert("The task with the selected ID was not found.")
@@ -28,14 +37,27 @@ export class TaskControllerClass {
 
     async getTask(taskId: string, projectId: string) {
         const taskList = await this.getTaskList(projectId);
+        if (taskList === undefined) {
+            window.alert("An error occurred while trying to get the task list.")
+            return;
+        };
+
         const task = taskList.find(task => task.id === taskId);
         return task;
     }
 
     async setTask(taskId: string, projectId: string, updatedTask: Task) {
         const updatedProject = await ProjectController.getProject(projectId);
+        if (updatedProject === undefined) {
+            window.alert("An error occurred while trying to get the task list.")
+            return;
+        }
 
         const taskIndex = updatedProject.tasks.findIndex(task => task.id === taskId);
+        if (taskIndex === -1) {
+            window.alert("The task with the selected ID was not found.")
+            return;
+        }
         updatedProject.tasks[taskIndex] = updatedTask;
 
         await ProjectController.setProject(projectId, updatedProject);
@@ -43,11 +65,21 @@ export class TaskControllerClass {
 
     async getTaskList(projectId: string) {
         const project = await ProjectController.getProject(projectId);
+        if (project === undefined) {
+            window.alert("An error occurred while trying to get the task list.")
+            return;
+        }
+
         return project.tasks;
     }
 
     async setTaskList(projectId: string, updatedTaskList: Task[],) {
         const updatedProject = await ProjectController.getProject(projectId);
+        if (updatedProject === undefined) {
+            window.alert("An error occurred while trying to set the task list.")
+            return;
+        }
+
         updatedProject.tasks = updatedTaskList;
 
         await ProjectController.setProject(projectId, updatedProject);
