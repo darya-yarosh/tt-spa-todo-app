a0ccdcb361af71a4ca446f66a17537f13bfda42c
darya-yarosh <gurimusagi@yandex.ru>
2023-10-02 03:02:24 +0300

    Updated form component of project

--- src/components/Project/ProjectForm/ProjectForm.tsx
+++ src/components/Project/ProjectForm/ProjectForm.tsx
@@ -1,42 +1,50 @@
 import { useState } from "react";
 
+import Project from "models/Project";
 import { BUTTON, FormOperation } from "models/Interface";
 
 import ButtonIcon from "components/General/ButtonIcon/ButtonIcon";
-
 import returnIcon from "images/buttons/return.svg";
 import confirmIcon from "images/buttons/confirm.svg";
 
 import styles from "components/Project/ProjectForm/ProjectForm.module.scss"
 
 interface ProjectFormProps {
-    operationTitle: FormOperation
-    sendForm: (projectTitle: string) => void;
+    project?: Project,
+    operationTitle: FormOperation,
+    sendForm: (newProject: Project) => void;
     closeForm: () => void;
 }
 
 export default function ProjectForm({
+    project,
     operationTitle,
+    sendForm,
     closeForm,
-    sendForm
 }: ProjectFormProps) {
-    const [projectTitle, setProjectTitle] = useState<string>("");
+    const [projectTitle, setProjectTitle] = useState<string>(project?.title || "");
 
     function isCorrectProject() {
-        return projectTitle.length > 0;
+        return projectTitle.trim().length > 0;
     }
 
     function handlerSendForm() {
-        if (isCorrectProject()) {
-            sendForm(projectTitle);
-            closeForm();
+        if (!isCorrectProject()) {
+            window.alert("Project title is empty. Please write something in title.");
+            return;
         }
-        else {
-            window.alert("Incorrect project name.")
+
+        const newProject: Project = {
+            id: project?.id || crypto.randomUUID(),
+            title: projectTitle,
+            tasks: project?.tasks || []
         }
+
+        sendForm(newProject);
+        closeForm();
     }
 
-    return <form className={styles.wrapper} 
+    return <form className={styles.wrapper}
         onSubmit={(event) => {
             event.preventDefault();
             handlerSendForm();
@@ -45,10 +53,10 @@ export default function ProjectForm({
             <ButtonIcon iconSVG={returnIcon} caption={BUTTON.return} onClick={closeForm} />
             <p className={styles.title}>{operationTitle} project:</p>
         </div>
-        <span className={styles.line}/>
+        <span className={styles.line} />
         <div className={styles.content}>
-            <span className={styles.span}>
-                <label className={styles.label}>Title:</label>
+            <span>
+                <label>Title:</label>
                 <input className={styles.input_string} type="text"
                     placeholder="№1 Project name"
                     value={projectTitle}
